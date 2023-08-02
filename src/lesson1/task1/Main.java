7b561d56bd13d531360fd63e1edc68c9d316d281
krlld <kirilldikun25@gmail.com>
2023-08-02 22:25:55 +0300

    add comments

--- src/lesson1/task1/Main.java
+++ src/lesson1/task1/Main.java
@@ -1,6 +1,9 @@
 package lesson1.task1;
 
 public class Main {
+    //    Задача 1:
+    //    Написать приложение, которое будет вычислять и выводить значение по формуле:
+    //    a=4*(b+c-1)/2; b и c задаем в коде самостоятельно.
     public static void main(String[] args) {
         int b = 3, c = 5;
         float a = 4 * (b + c - 1) / 2f;
