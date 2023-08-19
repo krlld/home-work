881db006e6d876c34b627d3377de9422c088a156
krlld <kirilldikun25@gmail.com>
2023-08-19 23:34:14 +0300

    Make lesson 6, task 2 with recursion

--- src/lesson6/task2/ATM.java
+++ src/lesson6/task2/ATM.java
@@ -25,7 +25,31 @@ public class ATM {
         this.count20 += count20;
     }
 
+    private class BanknotesSet {
+        private int count100 = 0;
+        private int count50 = 0;
+        private int count20 = 0;
+
+        public int getSum() {
+            return count100 * 100 + count50 * 50 + count20 * 20;
+        }
+
+        public String toString() {
+            return "Sum: " + getSum() + "\n100: " + count100 + ", 50: " + count50 + ", 20: " + count20;
+        }
+    }
+
     public boolean isPossibleToWithdraw(int money) {
+        BanknotesSet banknotesSet = new BanknotesSet();
+        withdraw(money, banknotesSet);
+        if (banknotesSet.getSum() == money) {
+            System.out.println(banknotesSet.toString());
+            return true;
+        }
+        return false;
+    }
+
+    private void withdraw(int money, BanknotesSet banknotesSet) {
         for (int i = 0; i <= count20; i++) {
             if (i * 20 > money) {
                 break;
@@ -39,12 +63,36 @@ public class ATM {
                         break;
                     }
                     if (i * 20 + j * 50 + k * 100 == money) {
-                        System.out.printf("100: %d, 50: %d, 20: %d", k, j, i);
-                        return true;
+                        banknotesSet.count20 = i;
+                        banknotesSet.count50 = j;
+                        banknotesSet.count100 = k;
+                        return;
                     }
                 }
             }
         }
+    }
+
+    public boolean isPossibleToWithdrawRecursion(int money) {
+        BanknotesSet banknotesSet = new BanknotesSet();
+        withdrawRecursion(money, banknotesSet);
+        if (banknotesSet.getSum() == money) {
+            System.out.println(banknotesSet.toString());
+            return true;
+        }
         return false;
+    }
+
+    private void withdrawRecursion(int money, BanknotesSet banknotesSet) {
+        if (banknotesSet.count100 < count100 && money >= 100) {
+            banknotesSet.count100++;
+            withdrawRecursion(money - 100, banknotesSet);
+        } else if (banknotesSet.count50 < count50 && money >= 50) {
+            banknotesSet.count50++;
+            withdrawRecursion(money - 50, banknotesSet);
+        } else if (banknotesSet.count20 < count20 && money >= 20) {
+            banknotesSet.count20++;
+            withdrawRecursion(money - 20, banknotesSet);
+        }
     }
 }
--- src/lesson6/task2/Main.java
+++ src/lesson6/task2/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         ATM atm = new ATM(5, 6, 7);
         atm.isPossibleToWithdraw(250);
+        atm.isPossibleToWithdrawRecursion(250);
     }
 }
